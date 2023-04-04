5f110d1a180b086e1570a73e5d8ff1c77b17bd8a
unknown <how951212@gmail.com>
2023-04-04 14:54:57 +0800

    end of product controller js session on 20230404

--- fsd_frontend/WebDemo2/js/productController.js
+++ fsd_frontend/WebDemo2/js/productController.js
@@ -32,6 +32,16 @@ const product2 = {
 const productList = [];
 productList.push(product1, product2);
 
+const displayDetails = index => {
+    
+    //When user clicks on any "More" button, the details of the selected product will be displayed
+    document.querySelector("#modalName").innerHTML = productList[index].name;
+    document.querySelector("#modalDesc").innerHTML = productList[index].description;
+    document.querySelector("#modalImg").src = productList[index].imageURL;
+    document.querySelector("#modalStyle").innerHTML = productList[index].style;
+    document.querySelector("#modalPrice").innerHTML = productList[index].price;
+}
+
 // 3) Display all products when user launch the product.html page
 const displayProduct = () => {
     let display = "";
@@ -45,7 +55,7 @@ const displayProduct = () => {
                     <div class="card-body">
                         <h5 class="card-title">${productList[i].name}</h5>
                         <p class="card-text">${productList[i].style}</p>
-                        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#productModal">More</a>
+                        <a id="item${i+1}" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#productModal" onClick="displayDetails(${i})">More</a>
                     </div>
                 </div>
             </div>
@@ -53,8 +63,36 @@ const displayProduct = () => {
     }
 
     document.querySelector("#row").innerHTML = display;
-
 } //End of displayProduct fn
+
+
+
+// 4) Add product to product list when user clicks on the submit button from productform.html
+// Add product fn
+const addProduct = (prdtName, prdtDescription, prdtImageURL, prdtStyle, prdtPrice) => {
+    /*
+    The parameters will be sent in from another fn thru the arguments
+    1. Construct/Create the product
+        property: value (apparently standard is to use same name for both i.e. parameters same names as object properties)
+    */
+
+    const productItem = {
+        name: prdtName,
+        description: prdtDescription,
+        imageURL: prdtImageURL,
+        style: prdtStyle,
+        price: prdtPrice
+    }
+
+    productList.push(productItem);
+}
+
+//The individual property value are sent in thru the argument - need to refactor when have productForm.html
+addProduct("Blue T-shirt", "This is a blue T-Shirt", "http://www.jennybeaumont.com/wp-content/uploads/2015/03/placeholder.gif", "Flower print", 35);
+addProduct("Red T-shirt", "This is a red T-Shirt", "http://www.jennybeaumont.com/wp-content/uploads/2015/03/placeholder.gif", "Flower print", 25);
+addProduct("White T-shirt", "This is a white T-Shirt", "http://www.jennybeaumont.com/wp-content/uploads/2015/03/placeholder.gif", "Flower print", 40);
+
+
 
 // Call displayProduct fn
 displayProduct();
