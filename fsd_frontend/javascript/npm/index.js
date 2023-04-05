d1ed217cb8b89788f742e6d353ee899fb46108b7
unknown <how951212@gmail.com>
2023-04-05 22:37:55 +0800

    minor edits to index.js for notes and readability

--- fsd_frontend/javascript/npm/index.js
+++ fsd_frontend/javascript/npm/index.js
@@ -1,12 +1,28 @@
+// provides prompt() function to prompt for input and store into a variable
 var prompt = require('prompt-sync')({sigint: true});
-const clear = require('clear-screen');
 
-//Create some global variables
+// provides clear() fn to Clear your console buffer
+const clear = require('clear-screen'); 
+
+// Create some global variables (remember: const variables cannot be changed later)
 const space = '_';
 const row = 2;
 const column = 3;
-const field = []; //an empty array
-//https://www.freecodecamp.org/news/javascript-2d-arrays/
+const field = []; //create an empty array first (can make it 2D later or can create 2D straight away)
+// https://www.freecodecamp.org/news/javascript-2d-arrays/
+
+/*
+to reference data in 2D-array: field[#1][#2],
+
+#1 = row; index of big array (i.e. which [i#] in [ [i1], [i2], [i3]...])
+
+#2 = column; index of inner array (i.e. which element in array [i#])
+
+visualise: [[1, 2, 3],
+            [a, b, c]]; 
+            
+#1 will be [1, 2, 3] or [a, b, c]. I trust myself to figure out what #2 will be...
+*/
 
 const word = "MADCAT";
 
@@ -26,18 +42,20 @@ const generateField = () =>
             field[i][col] = space; //loop "column"-times; here is 3 times, so ['_', '_', '_']
         }
         //console.log(field); //print row-by-row
-    } //Enf of both Outer Loop
-    //result is probably supposed to be [ '_', '_', '_' ]
-    //                                  [ '_', '_', '_' ]
+    } //End of both Outer Loop
+
+    //field = [[ '_', '_', '_' ], [ '_', '_', '_' ]]
 
     // getWord = field.toString();
     // console.log(getWord);
 
 } //End of generateField function
 
+
+
 const print = () =>
 {
-    clear();
+    clear(); //Clear your console buffer
 
     const displayString = field.map(row => 
         {
@@ -55,18 +73,20 @@ in print() fn above, map method loops thru
 - then each row and joing the elements with next line (i.e. '\n')
 */
 
+
+
 const askQuestion = () =>
 {
     //prompt
     const row = prompt('Enter Row: ');
-    const column = prompt('Enter Col: ');
+    const column = prompt('Enter Column: ');
     const letter = prompt('Enter a letter: ').toUpperCase();
 
     //Change array element to the letter based on user input
     field[row][column] = letter;
-
-
 }
+
+
 
 const startGame = () =>
 {
@@ -90,7 +110,7 @@ const startGame = () =>
         }
     }
 
-}
+} //End of startGame() function
 
 generateField(); //Call to generate 2D array field
-startGame(); //Call to get input from user
+startGame(); //Call to start game
